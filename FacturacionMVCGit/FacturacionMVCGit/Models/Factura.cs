7688b2718e0c6db2154536d68d011911dbb93714
Carlos Enrique Lemus Serrano <kike_lemus@hotmail.com>
2019-02-22 11:10:11 -0600

    cambios en factura se adiciono: IdProducto, IdProveedor, Fecha y metodo CalcularTotal

--- FacturacionMVCGit/FacturacionMVCGit/Models/Factura.cs
+++ FacturacionMVCGit/FacturacionMVCGit/Models/Factura.cs
@@ -8,6 +8,15 @@ namespace FacturacionMVCGit.Models
     public class Factura
     {
         public int Id { get; set; }
+        public int IdProducto { get; set; }
+        public int IdProveedor { get; set; }
         public string Nit { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public decimal CalcularTotal(decimal subtotal)
+        {
+            return subtotal -= subtotal * (decimal)0.13;
+        }
+
     }
 }
